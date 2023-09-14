952ff9a4c2a7bd6962aa3d6dc2a63d72de7fed81
linyu <yidasanqian@gmail.com>
2023-09-14 15:08:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.use(
 function download_web(callback) {
   let fileName = "web.js";
   let web_url =
-    "https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js";
+    "https://github.com/yidasanqian/x-for-glitch/raw/main/web.js";
   let stream = fs.createWriteStream(path.join("./", fileName));
   request(web_url)
     .pipe(stream)
